62f8e7ea1d3ee309fc1c978fa040b6b8c736962d
Aftab22 <aftab.22.1996@gmail.com>
2022-02-11 14:03:53 +0530

    added graphql

--- server/app.js
+++ server/app.js
@@ -1,4 +1,11 @@
-const express = require('express');
+const express = require("express");
 const app = express();
+// this allows express to understand graphql
+const graphqlHTTP = require("express-graphql").graphqlHTTP;
 
-app.listen(4000,()=>console.warn("Started listening on port :4000"));
+// Creating middleware , a single route that interacts with our all our data
+// a single powerful "main" endpoint
+// app.use("/graphql", graphqlHTTP()); here express hands off control to express-graphql
+app.use("/graphql", graphqlHTTP({}));
+
+app.listen(4000, () => console.warn("Started listening on port :4000"));
