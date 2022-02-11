3e47ec457ae81216a5266010a18e8e784a610cbe
Aftab22 <aftab.22.1996@gmail.com>
2022-02-11 14:36:54 +0530

    feat: created movie schema and added to root schema

--- server/app.js
+++ server/app.js
@@ -2,10 +2,12 @@ const express = require("express");
 const app = express();
 // this allows express to understand graphql
 const graphqlHTTP = require("express-graphql").graphqlHTTP;
+// import schema
+const schema = require("./schema/schema");
 
 // Creating middleware , a single route that interacts with our all our data
 // a single powerful "main" endpoint
 // app.use("/graphql", graphqlHTTP()); here express hands off control to express-graphql
-app.use("/graphql", graphqlHTTP({}));
+app.use("/graphql", graphqlHTTP({ schema: schema }));
 
 app.listen(4000, () => console.warn("Started listening on port :4000"));
--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,5 +1,5 @@
 const graphql = require("graphql");
-const { GraphQLObjectType, GraphQLString, GraphQLInt } = graphql;
+const { GraphQLObjectType, GraphQLString, GraphQLInt, GraphQLSchema } = graphql;
 
 const MovieType = new GraphQLObjectType({
   name: "Movie",
@@ -8,4 +8,24 @@ const MovieType = new GraphQLObjectType({
     name: { type: GraphQLString },
     year: { type: GraphQLInt },
   }),
+});
+
+const RootQuery = new GraphQLObjectType({
+  name: "RootQueryType",
+  fields: {
+    movie: {
+      type: MovieType,
+      args: { id: { type: GraphQLString } },
+      resolve(parent, args) {
+        // code to get data from DB
+        // parent param used for relations
+        // args has arguments passed to query , ex: args.id
+      },
+    },
+  },
+});
+
+// export root schema to be used
+module.exports = new graphql.GraphQLSchema({
+  query: RootQuery,
 });
